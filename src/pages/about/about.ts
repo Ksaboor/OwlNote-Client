00d17fdb91cf6081edab03318b7990811e485e57
Khail Saboor <khalilsaboor1@gmail.com>
2018-08-30 17:05:27 -0400

    font fix

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -8,7 +8,23 @@ import { NavController } from 'ionic-angular';
 export class AboutPage {
 
   constructor(public navCtrl: NavController) {
-
+    this.getQuestion();
+    questions: any[];
+  }
+  getQuestion(){
+    this.http.get('http://localhost:8080/questions/Getquestions').subscribe(
+			(response: Array<Question>) => {
+				console.log('get questions response', response);
+				this.questions = response;
+        this.currentQuestion = response[0].question;
+			},
+			(err) => {
+				console.error('get question error', err);
+			},
+			() => {
+				console.log('get questions');
+			}
+		);
   }
 
 }
